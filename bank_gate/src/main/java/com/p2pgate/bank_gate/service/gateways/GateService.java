a36942a12ad7a62e5743903d6a404582161e5b3b
koa73 <koa73@bk.ru>
2020-01-03 17:49:36 +0300

    -- add getSumWithoutFee

--- bank_gate/src/main/java/com/p2pgate/bank_gate/service/gateways/GateService.java
+++ bank_gate/src/main/java/com/p2pgate/bank_gate/service/gateways/GateService.java
@@ -6,6 +6,9 @@ import com.p2pgate.lib.domain.PayFee;
 import com.p2pgate.bank_gate.rest.exception.PayApiException;
 import com.p2pgate.bank_gate.rest.view.FeeResponse;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 
 /**
  *
@@ -16,4 +19,21 @@ public interface GateService {
     FeeResponse getFee(PayFee request) throws PayApiException;
     String sendPay(PayAccept request, String payCallback) throws PayApiException;
     String sendConfirm(String md, String PaRes) throws PayApiException;
+
+    default float getSumWithoutFee(float sum, float minFee, float feePercent){
+        float fee, tmp = sum ;
+
+        do {
+            fee = Math.max(minFee,(tmp * feePercent/100));
+            tmp = sum - fee;
+            if (fee == 30)
+                break;
+
+        } while (Math.abs(tmp*(feePercent/100+1) -sum)>.01);
+
+        if ((tmp*(feePercent/100+1) -sum) >0)
+            tmp -=.01f;
+
+        return new BigDecimal(tmp).setScale(2, RoundingMode.HALF_EVEN).floatValue();
+    }
 }
